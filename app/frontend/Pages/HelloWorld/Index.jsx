26bf9c14f63b7366649b7e13f08443e645de2d1d
Jonathan Davies <jonathan.davies@mmtm.io>
2022-02-12 15:25:13 +0000

    adds inertiajs

---
+++ app/frontend/Pages/HelloWorld/Index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function ({ hello }) {
+  return (
+    <div className="bg-gray-50 text-blue-600 container mx-auto">
+      here is some jsx {hello} wef
+      <div className="">let's add something new</div>
+    </div>
+  );
+}
