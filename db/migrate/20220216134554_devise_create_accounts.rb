1798b7990a0928a1dfa775580656cad65a64a233
Jonathan Davies <jonathan.davies@mmtm.io>
2022-02-17 23:01:36 +0000

    fix single quotes globally

--- db/migrate/20220216134554_devise_create_accounts.rb
+++ db/migrate/20220216134554_devise_create_accounts.rb
@@ -4,8 +4,8 @@ class DeviseCreateAccounts < ActiveRecord::Migration[7.0]
   def change
     create_table :accounts do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :email,              null: false, default: ''
+      t.string :encrypted_password, null: false, default: ''
 
       ## Account Name
       t.string :first_name
