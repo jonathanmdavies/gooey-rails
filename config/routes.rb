9bf9c702b65b50f0e9aecb8aee13a2ad68725cce
Jonathan Davies <jonathan.davies@mmtm.io>
2022-02-17 16:23:08 +0000

    Generate Devise controllers to can be overridden with Inertia

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :accounts
+  devise_for :accounts, controllers: {
+    sessions: 'accounts/sessions'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
