6c2025e2b48571893e138a2bf1663240d75ee4bf
Jonathan Davies <jonathan.davies@mmtm.io>
2022-02-17 16:24:05 +0000

    Configure Inertia to handle CSRF Token via Axios

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  include InertiaCsrf
+
+
+  inertia_share flash: -> {
+    {
+      success: flash.notice,
+      alert: flash.alert,
+    }
+  }
+
 end
