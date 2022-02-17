a355e89b948c79365ee11e1e885172696f5aaba4
Jonathan Davies <jonathan.davies@mmtm.io>
2022-02-17 14:14:28 +0000

    update factories to use Faker data

--- spec/factories/accounts.rb
+++ spec/factories/accounts.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :account do
-    first_name { 'John' }
-    last_name { 'Smith' }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
     sequence(:email) { |n| "account#{n}@example.com" }
     password { 'password' }
   end
