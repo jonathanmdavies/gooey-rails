43952927025dc84c73e180aa2a38bdc887122c80
Jonathan Davies <jonathan.davies@mmtm.io>
2022-02-18 15:50:22 +0000

    implement blurry bg logo thing

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,18 @@ module.exports = {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
         mono: ["IBM Plex Mono", "monospace"],
       },
+      keyframes: {
+        move: {
+          "0%": { transform: `translate(0px, 0px) scale(1)` },
+          "25%": { transform: `translate(60px, 0px) scale(1.2)` },
+          "50%": { transform: `translate(60px, 60px) scale(1)` },
+          "75%": { transform: `translate(0px, 60px) scale(1.2)` },
+          "100%": { transform: `translate(0px, 0px) scale(1)` },
+        },
+      },
+      animation: {
+        move: "move 6s ease-in-out infinite",
+      },
     },
   },
   plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
