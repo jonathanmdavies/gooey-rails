71a473811de6375b2ecb5b561ead1ddf60e3aa2e
Jonathan Davies <jonathan.davies@mmtm.io>
2022-02-23 11:18:18 +0000

    Enables Hot Module Reloading with React

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import RubyPlugin from "vite-plugin-ruby";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   build: { sourcemap: false },
-  plugins: [RubyPlugin()],
+  plugins: [RubyPlugin(), react()],
 });
