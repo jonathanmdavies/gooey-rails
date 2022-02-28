3c6dd9b933bd960c797565e229527a6085a278eb
Jonathan Davies <jonathanmdavies@me.com>
2022-02-28 14:49:37 +0000

    Extracts a Dropdown component
    - Header and Feed Index now leverage general Dropdown
    component that can be customised
    - Pass in styled button as a prop
    - primary and secondary items with different styles
    - position with position css props (tailwind css)
    - Also adds dynamic Icon HOC

--- app/frontend/Layouts/Authenticated.jsx
+++ app/frontend/Layouts/Authenticated.jsx
@@ -1,11 +1,12 @@
-import React, { Fragment } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { usePage, InertiaLink } from "@inertiajs/inertia-react";
-import { Menu, Transition } from "@headlessui/react";
-import { CogIcon, LogoutIcon, UserIcon } from "@heroicons/react/solid";
-import FlashMessages from "@/components/FlashMessages";
+import { CogIcon } from "@heroicons/react/solid";
+
 import { destroy_account_session_path, feeds_path, root_path } from "@/routes";
+import FlashMessages from "@/components/FlashMessages";
 import NewFeedButton from "@/components/NewFeedButton";
+import Dropdown from "@/components/Dropdown";
 
 export default function Authenticated({ children }) {
   return (
@@ -18,56 +19,46 @@ export default function Authenticated({ children }) {
 }
 
 function Header() {
-  const { current_account } = usePage().props;
+  const {
+    current_account: { email },
+  } = usePage().props;
   const { url } = usePage();
+
+  const primaryNavigationItems = [
+    {
+      label: email,
+      icon: "UserIcon",
+      href: "/",
+      method: "get",
+    },
+  ];
+
+  const secondaryNavigationItems = [
+    {
+      label: "Log out",
+      icon: "LogoutIcon",
+      href: destroy_account_session_path(),
+      method: "delete",
+    },
+  ];
 
   return (
     <header className="flex items-center justify-between border-b border-slate-100 bg-white py-2 px-4">
       <div className="flex w-40 items-center">
-        <Menu as="div" className="relative h-8">
-          <Menu.Button
-            id="settings-menu"
-            className="mr-3 flex h-8 w-8 items-center justify-center rounded-full bg-gradient-to-br from-slate-100 to-slate-200 transition active:scale-95"
-          >
-            <CogIcon className="h-6 w-6 text-slate-600" />
-            <span className="sr-only">Settings</span>
-          </Menu.Button>
-          <Transition
-            as={Fragment}
-            enter="transition ease-out duration-100"
-            enterFrom="transform opacity-0 scale-95"
-            enterTo="transform opacity-100 scale-100"
-            leave="transition ease-in duration-75"
-            leaveFrom="trasform opacity-100 scale-100"
-            leaveTo="transform opacity-0 scale-95"
-          >
-            <Menu.Items className="absolute left-0 z-10 mt-1 w-56 origin-top-left overflow-hidden rounded-lg border border-slate-100 bg-white shadow-sm">
-              <div className="">
-                <Menu.Item>
-                  <a
-                    href="/"
-                    className="group flex items-center px-4 py-2 font-mono text-sm font-medium text-slate-700 transition hover:bg-slate-50"
-                  >
-                    <UserIcon className="mr-2 h-4 w-4 text-slate-500" />
-                    {current_account.email}
-                  </a>
-                </Menu.Item>
-
-                <Menu.Item>
-                  <InertiaLink
-                    href={destroy_account_session_path()}
-                    method="delete"
-                    as="button"
-                    className="group flex w-full items-center border-t border-slate-100 px-4 py-2 font-mono text-sm font-medium text-rose-700 transition hover:bg-rose-50"
-                  >
-                    <LogoutIcon className="mr-2 h-4 w-4 text-rose-700" />
-                    Log out
-                  </InertiaLink>
-                </Menu.Item>
-              </div>
-            </Menu.Items>
-          </Transition>
-        </Menu>
+        <Dropdown
+          button={
+            <button
+              type="button"
+              className="settings-menu-button mr-3 flex h-8 w-8 items-center justify-center rounded-full bg-gradient-to-br from-slate-100 to-slate-200 transition active:scale-95"
+            >
+              <CogIcon className="h-6 w-6 text-slate-600" />
+              <span className="sr-only">Settings</span>
+            </button>
+          }
+          primaryItems={primaryNavigationItems}
+          secondaryItems={secondaryNavigationItems}
+          position="left-0 z-10 mt-1"
+        />
       </div>
 
       <div className="flex items-center space-x-2">
