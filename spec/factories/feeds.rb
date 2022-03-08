a2c85e269973d78cb0caa21c35a0b6504d383ffb
Jonathan Davies <jonathanmdavies@me.com>
2022-03-08 15:26:10 +0000

    Adds trait for feed with items

--- spec/factories/feeds.rb
+++ spec/factories/feeds.rb
@@ -5,5 +5,15 @@ FactoryBot.define do
     name { Faker::App.name }
     last_fetched_at { Faker::Time.between(from: 1.day.ago, to: Time.now) }
     association :account
+
+    factory :feed_with_items do
+      transient do
+        items_count { 1 }
+      end
+
+      after(:create) do |feed, evaluator|
+        create_list(:item, evaluator.items_count, feed: feed)
+      end
+    end
   end
 end
