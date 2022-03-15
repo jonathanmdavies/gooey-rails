9956de57ff54baf8b11de9cf5e2cbf0d17b18b59
Jonathan Davies <jonathanmdavies@me.com>
2022-03-15 13:12:49 +0000

    Users can select individual items to read

--- app/controllers/feeds/items_controller.rb
+++ app/controllers/feeds/items_controller.rb
@@ -8,9 +8,24 @@ class Feeds::ItemsController < ApplicationController
 
     render inertia: 'Unread/Index',
            props: {
-             items: items,
              feeds: feeds,
              feed: feed,
+             items: items,
+           }
+  end
+
+  def show
+    feeds = current_account.feeds.order(created_at: :desc).select(:id, :name)
+    feed = current_account.feeds.find(params[:feed_id])
+    items = feed.items.unread.order(published_at: :desc)
+    item = items.find { |i| i.id == params[:id].to_i }
+
+    render inertia: 'Unread/Index',
+           props: {
+             feeds: feeds,
+             feed: feed,
+             items: items,
+             item: item,
            }
   end
 end
--- spec/system/reading/view_unread_items_spec.rb
+++ spec/system/reading/view_unread_items_spec.rb
@@ -1,27 +1,59 @@
 require 'rails_helper'
 
-RSpec.describe 'Reading unread items', type: :system do
+RSpec.describe 'Reading Feeds', type: :system do
   before(:each) do
     @account = create(:account)
     login_as(@account)
   end
 
-  it 'shows all unread feed items' do
-    feed = create(:feed, account: @account)
-    item = create(:item, feed: feed)
+  context 'all items' do
+    it 'shows all unread feed items' do
+      first_feed = create(:feed_with_items, account: @account)
+      second_feed = create(:feed_with_items, account: @account)
+      first_feed_item = first_feed.items.first
+      second_feed_item = second_feed.items.first
 
-    visit items_path
+      visit items_path
 
-    expect(page).to have_content item.title
+      expect(page).to have_content(first_feed_item.title)
+      expect(page).to have_content(second_feed_item.title)
+      expect(page).to have_content "Select an item to start reading!"
+    end
+
+    it 'clicks to view an unread item' do
+      feed = create(:feed_with_items, account: @account, name: 'First Feed', items_count: 1)
+      item = feed.items.first
+
+      visit root_path
+      click_link item.title
+
+      expect(page).to have_selector 'h1', text: item.title
+      expect(page).to have_content item.content
+      expect(page.current_path).to eq item_path(feed.items.first)
+    end
   end
 
-  it 'shows all unread items for a feed' do
-    feed = create(:feed_with_items, account: @account, name: 'First Feed')
-    visit root_path
+  context 'individual feed' do
+    it 'shows all unread items' do
+      create(:feed_with_items, account: @account, name: 'First Feed')
+      visit root_path
 
-    click_link 'First Feed'
-    sleep(0.1) # Stops being flaky
+      click_link 'First Feed'
 
-    expect(page.current_path).to eq feed_items_path(feed)
+      expect(page).to have_content "Select an item to start reading!"
+    end
+
+    it 'clicks on item to view content' do
+      feed = create(:feed_with_items, account: @account, name: 'First Feed')
+      item = feed.items.first
+      visit root_path
+
+      click_link 'First Feed'
+      click_link item.title
+
+      expect(page).to have_selector 'h1', text: item.title
+      expect(page).to have_content item.content
+      expect(page.current_path).to eq feed_item_path(feed, item)
+    end
   end
 end
