7deeb9ababca08c53bf99ec962331cd707038e44
Jonathan Davies <jonathanmdavies@me.com>
2022-03-15 21:19:10 +0000

    Adds user resizing items column on x axis
    Adds a generic hook for resizing items on the x axis.
    Implemented for the items sidebar.

--- app/frontend/components/Reader/ItemsSidebar.jsx
+++ app/frontend/components/Reader/ItemsSidebar.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useRef } from "react";
 import { usePage, Link } from "@inertiajs/inertia-react";
+import Draggable from "react-draggable";
 import { item_path, feed_item_path } from "@/routes";
+import { useResizableX } from "@/utils/hooks";
 
 export default function ItemsSidebar() {
   const { items, feed } = usePage().props;
@@ -13,9 +15,25 @@ export default function ItemsSidebar() {
     return item_path(itemId);
   };
 
+  const resizeable = useRef(null);
+  const { start, resize, width, end } = useResizableX(
+    resizeable,
+    "items-column"
+  );
+
   return (
-    <div className=" w-80 overflow-y-scroll border-r border-slate-100">
-      <div className="divide-y divide-slate-100 overflow-y-scroll">
+    <div
+      ref={resizeable}
+      style={{ width }}
+      className="relative flex w-80 flex-row-reverse justify-between overflow-y-scroll border-r border-slate-100"
+    >
+      <div className="absolute right-0 h-full cursor-col-resize transition hover:bg-cyan-500/50">
+        <Draggable axis="x" onStart={start} onDrag={resize} onStop={end}>
+          <div className="h-full w-[3px]" />
+        </Draggable>
+      </div>
+
+      <div className="w-full divide-y divide-slate-100 overflow-y-scroll">
         {items.map(({ id, title, published_at }) => (
           <div key={id} className="w-full">
             <Link
