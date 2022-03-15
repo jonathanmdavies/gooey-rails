8b80a594e055344dd44ac2c72d7f301aad955b26
Jonathan Davies <jonathanmdavies@me.com>
2022-03-15 21:19:10 +0000

    Adds user resizing items column on x axis
    Adds a generic hook for resizing items on the x axis.
    Implemented for the items sidebar.

--- app/frontend/utils/hooks.js
+++ app/frontend/utils/hooks.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-export default function useIsVisible(ref) {
+export function useIsVisible(ref) {
   const [isVisible, setisVisible] = useState(false);
 
   const handleObserve = (entries) => {
@@ -23,4 +23,29 @@ export default function useIsVisible(ref) {
   }, [ref]);
 
   return isVisible;
+}
+
+export function useResizableX(ref, key) {
+  const getLocalState = () => localStorage.getItem(key);
+
+  const [initialPos, setInitialPos] = useState(null);
+  const [initialSize, setInitialSize] = useState(null);
+  const [width, setWidth] = useState(getLocalState);
+
+  const start = (e) => {
+    setInitialPos(e.clientX);
+    setInitialSize(ref.current.offsetWidth);
+  };
+
+  const resize = (e) => {
+    const diff = e.clientX - initialPos;
+    const newSize = initialSize + diff;
+    setWidth(`${newSize}px`);
+  };
+
+  const end = () => {
+    window.localStorage.setItem(key, width);
+  };
+
+  return { start, resize, end, width };
 }
