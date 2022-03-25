d85a91a264ca5245668ca6e0430b96f15088da23
Jonathan Davies <jonathanmdavies@me.com>
2022-03-25 14:57:53 +0000

    experiement with navigating

--- app/frontend/Pages/Dashboard/Show.jsx
+++ app/frontend/Pages/Dashboard/Show.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { usePage } from "@inertiajs/inertia-react";
 import {
   ChevronRightIcon,
@@ -9,15 +9,25 @@ import Authenticated from "@/Layouts/Authenticated";
 
 export default function Index() {
   const { items } = usePage().props;
+  const [activeItem, setActiveItem] = useState(items[0]);
 
   const scrollContainer = useRef(null);
 
   const goRight = (e) => {
+    if (activeItem.id === items[items.length - 1].id) {
+      return;
+    }
+    setActiveItem(items[items.indexOf(activeItem) + 1]);
     e.preventDefault();
     scrollContainer.current.scrollBy({ left: 480, behavior: "smooth" });
   };
 
   const goLeft = (e) => {
+    if (activeItem.id === items[0].id) {
+      return;
+    }
+
+    setActiveItem(items[items.indexOf(activeItem) - 1]);
     e.preventDefault();
     scrollContainer.current.scrollBy({
       left: -480,
@@ -34,21 +44,18 @@ export default function Index() {
               Welcome, Jonathan
             </h1>
             <p className="mt-1 text-sm font-medium text-slate-400">
-              12 Items Publised Today
+              12 Items Published Today
             </p>
           </div>
 
-          <ul className="flex divide-x">
-            <li className="px-2">
-              <div className="text-2xl font-bold text-slate-700">
-                150 <span className="text-sm text-slate-400">Items</span>
-              </div>
+          <ul className="flex divide-x divide-slate-200">
+            <li className="pr-4">
+              <div className="text-2xl font-bold text-slate-700">150</div>
               <div className="text-xs font-medium text-slate-400">
-                Read this month
+                Unread Items
               </div>
             </li>
-            <li className="px-2">
-              {" "}
+            <li className="pl-4">
               <div className="text-2xl font-bold text-slate-700">38</div>
               <div className="text-xs font-medium text-slate-400">
                 Unread Bookmarks
@@ -57,7 +64,7 @@ export default function Index() {
           </ul>
         </div>
 
-        <div className="my-12">
+        <div className="my-12 mx-auto">
           <div className="flex justify-between">
             <h2 className="text-xl font-semibold text-slate-800">
               Recently Published
@@ -87,7 +94,11 @@ export default function Index() {
             {items.map((item) => (
               <li
                 key={item.id}
-                className="relative h-48 w-[380px] flex-shrink-0 snap-start overflow-hidden rounded-xl bg-slate-100 p-8 shadow shadow-cyan-500/10"
+                className={`${
+                  activeItem.id === item.id
+                    ? "shadow-cyan-500/20"
+                    : "shadow-cyan-500/10"
+                } relative h-48 w-[380px] flex-shrink-0 snap-start overflow-hidden rounded-xl bg-slate-100 p-8 shadow transition`}
               >
                 <div className="z-20 flex h-full flex-col justify-between">
                   <div className="">
@@ -111,6 +122,19 @@ export default function Index() {
               </li>
             ))}
           </ul>
+
+          <div className="flex w-full justify-center">
+            <div className="flex space-x-2">
+              {items.map((item) => (
+                <div
+                  key={item.id}
+                  className={`${
+                    activeItem.id === item.id ? "bg-cyan-500" : "bg-slate-200"
+                  } h-2 w-2 rounded-full transition`}
+                />
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </Authenticated>
