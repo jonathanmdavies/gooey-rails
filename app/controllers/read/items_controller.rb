fba80598ad36db6b57544a2bf26118609926f53b
Jonathan Davies <jonathanmdavies@me.com>
2022-03-27 13:20:47 +0100

    Improves Item Panel layout
    – better margin and padding
    – ensures short item content goes full width
    – replaces item title with feed title for better context
    especially when viewing all items
    – clearer item title as part of prose

--- app/controllers/read/items_controller.rb
+++ app/controllers/read/items_controller.rb
@@ -12,7 +12,13 @@ class Read::ItemsController < ApplicationController
     feeds = current_account.feeds.order(created_at: :desc).select(:id, :name)
     items = current_account.items.order(published_at: :desc)
     item = items.find { |i| i.id == params[:id].to_i }
+    feed = item.feed
 
-    render inertia: 'Unread/Index', props: { items: items, feeds: feeds, item: item }
+    render inertia: 'Unread/Index', props: {
+      items: items,
+      feeds: feeds,
+      feed: feed,
+      item: item,
+    }
   end
 end
