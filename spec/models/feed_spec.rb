5d19514d369e805b55cdcf3277bab4a32ab949f3
Jonathan Davies <jonathanmdavies@me.com>
2022-05-09 14:16:45 +0100

    Moves location of feed counter cache spec.

--- spec/models/feed_spec.rb
+++ spec/models/feed_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Feed, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'unread items counter cache' do
+    it 'decrements counter cache when item is read' do
+      feed = create(:feed)
+      item = create(:unread_item, feed: feed)
+
+      item.update(read_at: Time.now)
+      feed.reload
+
+      expect(feed.unread_items_count).to eq(0)
+    end
+
+    it 'increments counter cache when item is unread' do
+      feed = create(:feed)
+      item = create(:read_item, feed: feed)
+
+      item.update(read_at: nil)
+      feed.reload
+
+      expect(feed.unread_items_count).to eq(1)
+    end
+  end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -28,26 +28,4 @@ RSpec.describe Item, type: :model do
       expect(Item.unread).to eq([unread])
     end
   end
-
-  context 'unread items counter cache' do
-    it 'decrements counter cache when item is read' do
-      feed = create(:feed)
-      item = create(:unread_item, feed: feed)
-
-      item.update(read_at: Time.now)
-      feed.reload
-
-      expect(feed.unread_items_count).to eq(0)
-    end
-
-    it 'increments counter cache when item is unread' do
-      feed = create(:feed)
-      item = create(:read_item, feed: feed)
-
-      item.update(read_at: nil)
-      feed.reload
-
-      expect(feed.unread_items_count).to eq(1)
-    end
-  end
 end
