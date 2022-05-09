b72b94d98178a1316bdf1d04d2df57fc4b5675c6
Jonathan Davies <jonathanmdavies@me.com>
2022-05-09 11:38:24 +0100

    Displays number of items published today

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,8 +3,12 @@ class DashboardsController < ApplicationController
 
   def show
     items = current_account.unread_items.includes(:feed).limit(6)
+    items_published_today = current_account.items.
+      where(published_at: Date.today.beginning_of_day..Date.today.end_of_day).
+      count
 
     render inertia: 'Dashboard/Show', props: {
+      items_published_today: items_published_today,
       items: Dashboard::ItemResource.new(items).serializable_hash,
     }
   end
