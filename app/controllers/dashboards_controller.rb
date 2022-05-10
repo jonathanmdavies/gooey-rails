7a693f92b10a53ce0ce8b85ef54414218996bd8e
Jonathan Davies <jonathanmdavies@me.com>
2022-05-10 15:52:29 +0100

    Adds Reading Habits graph and organises Dashboard
    Breaks out Dashboard into focused components.

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -8,6 +8,7 @@ class DashboardsController < ApplicationController
     bookmarks_count = current_account.bookmarks_count
     unread_bookmarks_count = current_account.unread_bookmarks_count
     bookmarked_items = current_account.items.bookmarked.includes(:feed).limit(5)
+    read_by_week = current_account.items.unscope(:order).group_by_week(:read_at, format: 'Week %U', last: 4).count
 
     render inertia: 'Dashboard/Show', props: {
       items_published_today: items_published_today,
@@ -16,6 +17,7 @@ class DashboardsController < ApplicationController
       unread_bookmarks_count: unread_bookmarks_count,
       items: Dashboard::ItemResource.new(items).serializable_hash,
       bookmarked_items: Dashboard::ItemResource.new(bookmarked_items).serializable_hash,
+      read_by_week: read_by_week,
     }
   end
 end
