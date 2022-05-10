1cce667e1b53745d32fb32efd3cd320d3335e8ac
Jonathan Davies <jonathanmdavies@me.com>
2022-05-10 12:31:25 +0100

    Adds Recently Bookmarked List & Heading Section

--- app/serializers/dashboard/item_resource.rb
+++ app/serializers/dashboard/item_resource.rb
@@ -3,7 +3,7 @@ class Dashboard::ItemResource
   include Rails.application.routes.url_helpers
   include ActionView::Helpers::DateHelper
 
-  attributes :id, :title, :published_at, :bookmarked_at
+  attributes :id, :title, :published_at, :bookmarked_at, :read_at
 
   attribute :published_at_in_words do |item|
     time_ago_in_words(item.published_at)
