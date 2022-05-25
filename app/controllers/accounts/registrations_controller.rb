d82c9a28a6b0405dbcb508b6005f4f475b07409d
Jonathan Davies <jonathanmdavies@me.com>
2022-05-25 11:54:23 +0100

    Adds user creating an account.

--- app/controllers/accounts/registrations_controller.rb
+++ app/controllers/accounts/registrations_controller.rb
@@ -1,18 +1,36 @@
 # frozen_string_literal: true
 
 class Accounts::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_permitted_parameters
 
   # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def new
+    render inertia: 'Accounts/Registrations/New', props: {}
+  end
 
-  # POST /resource
-  # def create
-  #   super
-  # end
+  # This controller is identical to the base controller except an inertia
+  # response on invalid sign up to return errors
+  def create
+    build_resource(sign_up_params)
+
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message! :notice, :signed_up
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      redirect_back fallback_location: new_account_registration_path, inertia: { errors: resource.errors }
+    end
+  end
 
   # GET /resource/edit
   # def edit
@@ -38,7 +56,7 @@ class Accounts::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
@@ -46,9 +64,9 @@ class Accounts::RegistrationsController < Devise::RegistrationsController
   # end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
