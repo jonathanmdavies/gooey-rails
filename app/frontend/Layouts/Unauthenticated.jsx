b2c7b943f4eae8dc74434296706b378ad7983467
Jonathan Davies <jonathanmdavies@me.com>
2022-05-25 11:48:07 +0100

    Improves design of unauthenticated layout

--- app/frontend/Layouts/Unauthenticated.jsx
+++ app/frontend/Layouts/Unauthenticated.jsx
@@ -7,20 +7,20 @@ export default function Unauthenticated({ children }) {
       <div className="flex flex-1 flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
         <div className="mx-auto w-full max-w-sm lg:w-96">
           <h1 className="mb-8 font-mono font-medium uppercase tracking-wide text-slate-800 lg:hidden">
-            Gooey Reader
+            RSS Reader
           </h1>
           <div className="">{children}</div>
         </div>
       </div>
       <div className="relative hidden w-0 flex-1 items-center justify-center lg:flex">
-        <h1 className="z-10 font-mono text-base font-semibold uppercase tracking-wide text-white opacity-80">
-          Reader
+        <img
+          src="/images/unauthenticated_background.jpg"
+          alt="background"
+          className="absolute inset-0 h-full w-full object-cover"
+        />
+        <h1 className="z-10 font-mono text-xl font-semibold uppercase tracking-wide text-white opacity-80">
+          RSS Reader
         </h1>
-        <div className="absolute h-40 w-40">
-          <div className="absolute h-40 w-40 transform-gpu animate-move rounded-full bg-cyan-500 opacity-50 mix-blend-multiply blur-xl" />
-          <div className="animation-delay-2000 absolute h-40 w-40 transform-gpu animate-move rounded-full bg-indigo-300 opacity-50 mix-blend-multiply blur-xl" />
-          <div className="animation-delay-4000 absolute h-40 w-40 transform-gpu animate-move rounded-full bg-rose-300 opacity-30 mix-blend-multiply blur-xl" />
-        </div>
       </div>
     </div>
   );
