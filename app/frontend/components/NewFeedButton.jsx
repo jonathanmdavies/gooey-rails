0ae7d0eb9bbf384c8aeea7e537c99d82527b2d27
Jonathan Davies <jonathanmdavies@me.com>
2022-06-23 14:49:57 +0100

    Adds resuable modal component
    Implements on new feed button

--- app/frontend/components/NewFeedButton.jsx
+++ app/frontend/components/NewFeedButton.jsx
@@ -1,15 +1,13 @@
-import React, { Fragment, useRef, useState } from "react";
-import { Dialog, Transition } from "@headlessui/react";
-import { RssIcon } from "@heroicons/react/solid";
+import React, { useState } from "react";
 import { useForm } from "@inertiajs/inertia-react";
 import { feeds_path } from "@/routes";
 import Input from "@/components/Base/Input";
 import Button from "./Base/Button";
 import IconButton from "./Base/IconButton";
+import Modal from "@/components/Modal/Modal";
 
 export default function NewFeedButton() {
   const [open, setOpen] = useState(false);
-  const cancelButtonRef = useRef(null);
 
   const {
     data,
@@ -52,93 +50,58 @@ export default function NewFeedButton() {
         onClick={() => setOpen(!open)}
       />
 
-      <Transition.Root show={open} as={Fragment}>
-        <Dialog
-          as="div"
-          className="fixed inset-0 z-10 overflow-y-auto"
-          initialFocus={cancelButtonRef}
-          onClose={() => closeModal()}
-        >
-          <div className="flex min-h-screen items-end justify-center px-4 pt-4 pb-20 text-center sm:block sm:p-0">
-            <Dialog.Overlay className="fixed inset-0 bg-slate-500 bg-opacity-50 backdrop-blur-sm transition-opacity " />
-            <span
-              className="hidden sm:inline-block sm:h-screen sm:align-middle"
-              aria-hidden="true"
-            >
-              &#8203;
-            </span>
-
-            <form
-              onSubmit={submit}
-              className="inline-block transform overflow-hidden rounded-lg bg-white text-left align-bottom shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:align-middle"
-            >
-              <div className="border-b border-slate-200 bg-gradient-to-tl from-slate-100 to-slate-50 p-6">
-                <div className="flex h-8 w-8 items-center justify-center rounded-full bg-gradient-to-br from-cyan-600 to-cyan-500">
-                  <RssIcon className="h-4 w-4 text-white" />
-                </div>
-                <div className="mt-2 sm:mt-4">
-                  <Dialog.Title
-                    as="h3"
-                    className="text-base font-medium text-slate-800"
-                  >
-                    Add a New Feed
-                  </Dialog.Title>
-                  <div className="mt-px">
-                    <p className="font-mono text-xs text-slate-500">
-                      Paste in a Feed URL
-                    </p>
-                  </div>
-                </div>
+      <Modal
+        open={open}
+        close={() => setOpen(false)}
+        title="Add a New Feed"
+        icon="RssIcon"
+        subtitle="Paste in a Feed URL."
+        submit={() => submit()}
+      >
+        <form onSubmit={submit}>
+          <div className="space-y-6">
+            <Input
+              label="Feed URL"
+              value={data.url}
+              type="text"
+              onChange={(e) => setData("url", e.target.value)}
+              name="name"
+              id="name"
+              errors={errors.url}
+            />
+            <div className="">
+              <div className="relative rounded-lg border border-slate-300 px-3 py-2 focus-within:border-cyan-600 focus-within:ring-0 focus-within:ring-cyan-600">
+                <label
+                  htmlFor="initially-fetch"
+                  className="absolute -top-2 left-2 -mt-px inline-block bg-white px-1 font-mono text-xs font-medium text-slate-800"
+                >
+                  Initially Fetch
+                </label>
+                <select
+                  id="initially-fetch"
+                  value={data.initially_fetch}
+                  onChange={(e) => setData("initially_fetch", e.target.value)}
+                  className="block w-full border-0 p-0 text-sm text-slate-800 placeholder-gray-500 focus:ring-0"
+                >
+                  <option value="one_day_ago">Last 24 Hours</option>
+                  <option value="three_days_ago">Last 3 Days</option>
+                  <option value="one_week_ago">Last 7 Days</option>
+                  <option value="two_weeks_ago">Last 2 Weeks</option>
+                </select>
               </div>
-              <div className="space-y-6 p-6">
-                <Input
-                  label="Feed URL"
-                  value={data.url}
-                  type="text"
-                  onChange={(e) => setData("url", e.target.value)}
-                  name="name"
-                  id="name"
-                  errors={errors.url}
-                />
-                <div className="">
-                  <div className="relative rounded-lg border border-slate-300 px-3 py-2 focus-within:border-cyan-600 focus-within:ring-0 focus-within:ring-cyan-600">
-                    <label
-                      htmlFor="initially-fetch"
-                      className="absolute -top-2 left-2 -mt-px inline-block bg-white px-1 font-mono text-xs font-medium text-slate-800"
-                    >
-                      Initially Fetch
-                    </label>
-                    <select
-                      id="initially-fetch"
-                      value={data.initially_fetch}
-                      onChange={(e) =>
-                        setData("initially_fetch", e.target.value)
-                      }
-                      className="block w-full border-0 p-0 text-sm text-slate-800 placeholder-gray-500 focus:ring-0"
-                    >
-                      <option value="one_day_ago">Last 24 Hours</option>
-                      <option value="three_days_ago" selected="selected">
-                        Last 3 Days
-                      </option>
-                      <option value="one_week_ago">Last 7 Days</option>
-                      <option value="two_weeks_ago">Last 2 Weeks</option>
-                    </select>
-                  </div>
-                </div>
-              </div>
-
-              <div className="p-6 pt-0 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
-                <Button color="cyan" disabled={processing} type="submit">
-                  Add Feed
-                </Button>
-                <Button type="button" onClick={() => closeModal()}>
-                  Cancel
-                </Button>
-              </div>
-            </form>
+            </div>
           </div>
-        </Dialog>
-      </Transition.Root>
+
+          <div className="pt-4 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
+            <Button color="cyan" disabled={processing} type="submit">
+              Add Feed
+            </Button>
+            <Button type="button" onClick={() => closeModal()}>
+              Cancel
+            </Button>
+          </div>
+        </form>
+      </Modal>
     </div>
   );
 }
