86eb72271275df9c0e43bea7107afecadec1f527
Jonathan Davies <jonathanmdavies@me.com>
2026-09-17 20:53:26 -0700

    Use check-circle icon for success messages
    Vex-Session: session-b543ba47c9c2a64296657458

--- app/frontend/components/FlashMessages.jsx
+++ app/frontend/components/FlashMessages.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { usePage } from "@inertiajs/inertia-react";
-import { EmojiHappyIcon, ExclamationIcon } from "@heroicons/react/solid";
+import { CheckCircleIcon, ExclamationIcon } from "@heroicons/react/solid";
 
 export default function FlashMessages() {
   const { flash } = usePage().props;
@@ -17,7 +17,7 @@ export default function FlashMessages() {
   if (flash.success) {
     return (
       <div className="mb-6 flex items-center rounded-lg bg-emerald-50 px-3 py-2 font-mono text-sm font-medium text-emerald-600">
-        <EmojiHappyIcon className="mr-1 h-4 w-4" />
+        <CheckCircleIcon className="mr-1 h-4 w-4" />
         {flash.success}
       </div>
     );
